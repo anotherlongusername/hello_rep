b383d503093fd755cbad8c4f65f58d90121992cb
Ivanov Dmitrii <id6800@mail.ru>
2025-03-23 23:30:37 +0300

    Initial commit

---
+++ src/main/java/com/example/forGitTest/ForGitTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.forGitTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForGitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForGitTestApplication.class, args);
+	}
+
+}
